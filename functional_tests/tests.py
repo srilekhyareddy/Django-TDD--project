946c0208960e6db83511f7d6595b9d69bf715267
Srilekhya Meda <srilekhyareddy@gmail.com>
2022-05-12 01:01:57 -0700

    Adding some changes to functional test on multi user usage

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,6 +90,12 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
+		# Francis visits the home page. There is no sign of Edith's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text 
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
 		# Francis starts creating a new list by entering a new item
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
